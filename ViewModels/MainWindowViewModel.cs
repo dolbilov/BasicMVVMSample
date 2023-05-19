2ca89f1cbd8d661ad98c47affca14ee03441fa2f
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-19 22:44:16 +0300

    chore: set up view and view model

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,33 @@
-﻿namespace BasicMVVMSample.ViewModels;
+﻿using System;
+using ReactiveUI;
+
+namespace BasicMVVMSample.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    #region Fields
+
+    private string? _name;
+
+    #endregion
+    
+    
+    public MainWindowViewModel()
+    {
+        this.WhenAnyValue(vm => vm.Name)
+            .Subscribe(_ => this.RaisePropertyChanged(nameof(Greeting)));
+    }
+
+    
+    #region Properties
+
+    public string? Name
+    {
+        get => _name;
+        set => this.RaiseAndSetIfChanged(ref _name, value);
+    }
+
+    public string Greeting => $"Hello, {(string.IsNullOrEmpty(Name) ? "anonymous" : Name)}";
+
+    #endregion
 }
